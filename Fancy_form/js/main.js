42aa1195521f394b9d58bc333a0e4c62f4c1dbd3
brizism <montielbriseida@gmail.com>
2018-06-18 19:37:52 -0400

    add field questions array

---
+++ Fancy_form/js/main.js
@@ -0,0 +1,8 @@
+// Questions Array
+const questions = [
+  { question: 'Enter Your First Name' },
+  { question: 'Enter Your Last Name' },
+  { question: 'Enter Your Email', pattern: /\S+@\S+\.\S+/ },
+  { question: 'Create a Password', type: 'password' }
+];
+
