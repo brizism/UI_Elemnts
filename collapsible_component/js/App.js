8d3bbcfa2b46ddffb292f6dc5d59f8725d3f2b1b
brizism <montielbriseida@gmail.com>
2018-07-08 17:45:16 -0400

    fetch randomusers and insert in collapsible component

--- collapsible_component/js/App.js
+++ collapsible_component/js/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import image from '../images/expand-vertical-4.svg';
+import image from '../images/cloud-upload-download-data-transfer.svg';
 import Collapsible from './Collapsible';
 
 class App extends React.Component {
+    state = {
+        isLoading: true,
+        contacts: []
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData(){
+        fetch('https://randomuser.me/api/?results=50&nat=us')
+            .then(res => res.json())
+            .then(data => data.results.map(user => (
+                {
+                    name: `${user.name.first} ${user.name.last}`,
+                    username: `${user.login.username}`,
+                    email: `${user.email}`,
+                    location: `${user.location.street}, ${user.location.city}`
+                }
+            )))
+            .then(contacts => this.setState({
+                contacts,
+                isLoading: false
+            }))
+            .catch(err => console.log(err))
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        localStorage.setItem('contacts', JSON.stringify(nextState.contacts))
+        localStorage.setItem('contactsDate', Date.now())
+    }
+    
+    
+
     render() {
+        const {isLoading, contacts} = this.state;
+        console.log(contacts)
         return (
             <div>
                 <header>
                     <img src={image} />
-                    <h1>Collapsible Content</h1>
+                    <h1>Contact List <button className="btn btn-sm btn-danger">Get more</button></h1>
                 </header>
-                <div className="content">
+                <div className={`content ${isLoading ? 'is-loading' : ''}`}>
                     <div className="panel-group">
-                        <Collapsible title="Overview">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel optio perspiciatis necessitatibus facilis ullam fuga beatae voluptate eligendi. Quaerat, nihil facilis? Repellendus architecto, voluptatem explicabo, iste eos rerum delectus quae tempore perspiciatis ipsam iure doloremque ex tempora voluptates repudiandae, odio et. Natus dolores aliquid ipsa dolor eaque hic laudantium nisi laboriosam nam quam perferendis iste quisquam quaerat nihil expedita delectus voluptates, consequatur consectetur excepturi incidunt, iusto error ducimus! Repellendus magni natus impedit voluptates dolore facere quia quam suscipit doloremque, magnam aut debitis accusantium asperiores saepe odio voluptate architecto? Ut necessitatibus a ullam illo alias. Officiis optio maxime rerum labore dolorem aut similique non eius, quo praesentium perferendis eum iste veniam aperiam fuga reiciendis cupiditate, distinctio consectetur impedit excepturi. Corrupti voluptas at voluptatibus illo aspernatur earum rem consequuntur temporibus numquam ex, fuga excepturi. Quasi consequuntur autem dolores reiciendis tempora. Voluptatem adipisci tempora qui laboriosam vel consectetur dolores quas enim libero magni?</p>
-                        </Collapsible>
-                        <Collapsible title="Features">
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, accusamus.</p>
-                        </Collapsible>
-                        <Collapsible title="Software">
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
-                        </Collapsible>
+                        {
+                            !isLoading && contacts.length > 0 ? contacts.map(contact => {
+                                const {name, username, email, location} = contact;
+                                return <Collapsible  key={username} title={name}>
+                                          <p>{email}<br />{location}</p>
+                                       </Collapsible>
+                            }) : null
+                        }
+                    </div>
+                    <div className="loader">
+                        <div className="icon"></div>
                     </div>
                 </div>
             </div>
