1bc24ea7a302496517d58a4116301b1185c88d31
brizism <montielbriseida@gmail.com>
2018-07-08 13:45:41 -0400

    add collapsible component

---
+++ collapsible_component/js/Collapsible.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Collapsible extends Component {
+  render() {
+    const {title, children} = this.props;
+    return (
+      <div className="panel">
+        <div className="panel-heading">
+          <h2>{title}</h2>
+        </div>
+        <div className="panel-collapse">
+          <div className="panel-body">
+              {children}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Collapsible.propTypes = {
+  title: PropTypes.string,
+};
+
+export default Collapsible;
